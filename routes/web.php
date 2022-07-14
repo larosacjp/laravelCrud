dcbe05d2815735fb01dbf487e0089fe572316735
larosacjp <larosa.cjp@gmail.com>
2022-07-14 11:31:55 +0800

    changed mail details in env file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route for mailing
+Route::get('/email', function(){
+  return new WelcomeMail();
+});
